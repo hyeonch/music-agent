8be962d1b4662b41fe97944ac4a5b0e578a6a063
hyeonchan <hyeonchan@pozalabs.com>
2025-09-29 01:48:41 +0900

    c: add precommit hooks

--- magent/service/usecases/recommend/container.py
+++ magent/service/usecases/recommend/container.py
@@ -1,5 +1,4 @@
 from dependency_injector import containers, providers
-from spotipy import Spotify
 
 from magent.adapters.repository.reccobeats import ReccobeatsRecommendationRepository
 from magent.adapters.repository.spotify import SpotifyArtistRepository
